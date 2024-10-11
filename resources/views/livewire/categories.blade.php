9183ba748cbaed276deb20d6c2741d33ff6f2975
Vasileios Ntoufoudis <info@ntoufoudis.com>
2024-10-12 00:41:34 +0300

    Add filters to Categories #27

--- resources/views/livewire/categories.blade.php
+++ resources/views/livewire/categories.blade.php
@@ -15,6 +15,8 @@
                     :sortColumn="$sortColumn"
                     :sortDirection="$sortDirection"
                     search="search"
+                    visibility="visibility"
+                    :filters="$filters"
                     create-modal="'showCreateModal'"
                     edit-modal="openEditModal"
                     delete-modal="confirmDelete"
